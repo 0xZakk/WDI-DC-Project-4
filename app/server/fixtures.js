7699dbf00dc298043c7db115b51ad05df1232deb
Zakk <zachary.fleischmann@gmail.com>
2015-05-31 12:59:23 -0400

    initial folder set up, set up first route to home page, begin auth

---
+++ app/server/fixtures.js
@@ -0,0 +1,13 @@
+if (Projects.find().count() ===0){
+  Projects.insert({
+    name: "Secret Layer"
+  });
+
+  Projects.insert({
+    name: "Batmobile"
+  });
+
+  Projects.insert({
+    name: "Wayne Manor"
+  });
+}
